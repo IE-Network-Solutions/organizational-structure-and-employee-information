f80753f4e94989721773dc873cf82d9aab1d489c
Selaml <selamawitlb@gmail.com>
2024-09-04 18:49:51 +0300

    PPII update user disable on firbase user

--- src/app/modules/users/user.service.ts
+++ src/app/modules/users/user.service.ts
@@ -271,14 +271,19 @@ export class UserService {
 
   async remove(id: string) {
     try {
-      await this.userRepository.findOneOrFail({ where: { id: id } });
+      const user = await this.userRepository.findOneOrFail({
+        where: { id: id },
+      });
+      await admin.auth().updateUser(user.firebaseId, {
+        disabled: true,
+      });
 
       return await this.userRepository.softRemove({ id });
     } catch (error) {
       if (error.name === 'EntityNotFoundError') {
         throw new NotFoundException(`User with id ${id} not found.`);
       }
-      throw error;
+      throw error?.message || error;
     }
   }
   async findReportingToUser(id: string) {
@@ -479,10 +484,21 @@ export class UserService {
       const user = await this.findOne(userId);
       if (user) {
         await this.userRepository.update(userId, { deletedAt: null });
+        await this.activateUserFromFirabse(user.firebaseId);
         return user;
       }
     } catch (error) {
-      throw new BadRequestException(error);
+      throw new BadRequestException(error.message);
+    }
+  }
+
+  async activateUserFromFirabse(firebaseId: string) {
+    try {
+      await admin.auth().updateUser(firebaseId, {
+        disabled: false,
+      });
+    } catch (error) {
+      throw new BadRequestException(error.message);
     }
   }
 }
