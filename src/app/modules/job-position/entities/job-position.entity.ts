ee5eb454371f4bb2b384a57bb32be8937547a1e5
Selaml <selamawitlb@gmail.com>
2025-03-24 13:59:52 +0300

    fix validation and nulable column

--- src/app/modules/job-position/entities/job-position.entity.ts
+++ src/app/modules/job-position/entities/job-position.entity.ts
@@ -3,12 +3,11 @@ import { Entity, Column, OneToMany, Unique } from 'typeorm';
 
 @Entity()
 @Unique(['name', 'tenantId'])
-
 export class JobPosition extends BaseModel {
   @Column()
   name: string;
   @Column({ nullable: true })
   description: string;
-@Column({ nullable: true })
+@Column()
   tenantId: string;
 }
